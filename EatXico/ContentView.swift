2a6f9c97b091e3a40c9c6c1d8557729371c25a03
OkapiGG <alancervantes800@gmail.com>
2025-09-30 10:50:54 -0600

    primer commit

---
+++ EatXico/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EatXico
+//
+//  Created by Alan Cervantes on 30/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EatXico/EatXicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatXicoApp.swift
+//  EatXico
+//
+//  Created by Alan Cervantes on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EatXicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
